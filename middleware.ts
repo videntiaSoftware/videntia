59b0f669034fb72787b0de282998255bdfb74f7a
videntiaSoftware <videntia.software@gmail.com>
2025-07-03 09:15:27 -0300

    corrijo el error en matcher de middleware

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Excluir todos los assets estáticos, imágenes, fuentes, archivos manifest, archivos de configuración, y todas las rutas /api/*
-    '/((?!_next/static|_next/image|favicon.ico|manifest.json|opengraph-image.png|twitter-image.png|robots.txt|sitemap.xml|site.webmanifest|browserconfig.xml|apple-touch-icon|android-chrome|mstile|.*\\.(svg|png|jpg|jpeg|gif|webp|ico|json|xml|txt|woff|woff2|ttf|eot)$|api/).*)',
+    // Excluye rutas de API, assets estáticos y archivos comunes
+    '/((?!api/|_next/|favicon.ico|manifest.json|opengraph-image.png|twitter-image.png|robots.txt|sitemap.xml|site.webmanifest|browserconfig.xml|apple-touch-icon|android-chrome|mstile).*)',
   ],
 };
