7ff48f42ba2bc1e37002ab177358a90f0acff706
fedeblau1989 <videntia.software@gmail.com>
2025-07-14 10:54:48 -0300

    propiedad para adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,12 @@ export default function RootLayout({
       <head>
         {/* Google AdSense Account Verification */}
         <meta name="google-adsense-account" content="ca-pub-4987669803086382" />
-        
+        {/* Google AdSense Verification Script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4987669803086382"
+          crossOrigin="anonymous"
+        ></script>
         {/* Google Search Console Verification */}
         {GSC_CONFIG.verificationMeta && (
           <meta name="google-site-verification" content={GSC_CONFIG.verificationMeta} />
